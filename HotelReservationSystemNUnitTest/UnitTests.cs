5a64ca2abda0800f38e3d48ccd2107bc2c28f24f
Mathura <mathuradas333@gmail.com>
2020-11-04 11:16:25 +0530

    [Mathura]Test. Tested WhenGiven_WrongHotelName_To_HotelDetails_ShouldThrow_HotelReservationException

--- HotelReservationSystemNUnitTest/UnitTests.cs
+++ HotelReservationSystemNUnitTest/UnitTests.cs
@@ -1,17 +1,31 @@
 using NUnit.Framework;
 using HotelReservationSystem;
+using System;
 
 namespace HotelReservationSystemNUnitTest
 {
     public class Tests
     {
         [Test]
-        public void WhenGiven_HotelName_To_HotelReservation_Should_AddHotel()
+        public void WhenGiven_HotelName_To_HotelDetails_Should_AddHotel()
         {
             bool expected = true;
             HotelDetails hotelDetailsTestObj = new HotelDetails();
-            bool result = hotelDetailsTestObj.AddHotel("Lakewood",110,90);
+            bool result = hotelDetailsTestObj.AddHotel("Lakewood", 110, 90);
             Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void WhenGiven_WrongHotelName_To_HotelDetails_ShouldThrow_HotelReservationException()
+        {
+            try
+            {
+                HotelDetails hotelDetailsTestObj = new HotelDetails();
+                bool result = hotelDetailsTestObj.AddHotel(null, 110, 90);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Invalid Hotel Name");
+            }
         }
     }
 }
