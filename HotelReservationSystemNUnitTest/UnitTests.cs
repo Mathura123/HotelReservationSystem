fcf24f22ed015b36db14cf006481d46f29722ec3
Mathura <mathuradas333@gmail.com>
2020-11-05 00:01:56 +0530

    [Mathura]Test. Tested WhenGiven_StartDate_And_EndDate_To_FindCheapestTotalRate_ShouldReturn_CheapestTotalRate

--- HotelReservationSystemNUnitTest/UnitTests.cs
+++ HotelReservationSystemNUnitTest/UnitTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using HotelReservationSystem;
 using System;
+using System.Collections.Generic;
 
 namespace HotelReservationSystemNUnitTest
 {
@@ -36,8 +37,8 @@ namespace HotelReservationSystemNUnitTest
             hotelDetailsTestObj.AddHotel("Bridgewood", 150, 50);
             hotelDetailsTestObj.AddHotel("Ridgewood", 220, 150);
             HotelReservation hotelReservationTestOj = new HotelReservation();
-            string result = hotelReservationTestOj.FindCheapestHotel(Convert.ToDateTime("10/09/2020"), Convert.ToDateTime("11/09/2020"));
-            Assert.AreEqual(expected, result);
+            List<string> result = hotelReservationTestOj.FindCheapestHotels(Convert.ToDateTime("10/09/2020"), Convert.ToDateTime("11/09/2020"));
+            Assert.AreEqual(expected, result[0]);
         }
         [Test]
         public void WhenGiven_StartDateGreaterThenEndDate_To_FindCheapestHotel_ShouldThrow_HotelReservationException()
@@ -49,12 +50,24 @@ namespace HotelReservationSystemNUnitTest
                 hotelDetailsTestObj.AddHotel("Bridgewood", 150, 50);
                 hotelDetailsTestObj.AddHotel("Ridgewood", 220, 150);
                 HotelReservation hotelReservationTestOj = new HotelReservation();
-                string result = hotelReservationTestOj.FindCheapestHotel(Convert.ToDateTime("16/09/2020"), Convert.ToDateTime("11/09/2020"));
+                List<string> result = hotelReservationTestOj.FindCheapestHotels(Convert.ToDateTime("16/09/2020"), Convert.ToDateTime("11/09/2020"));
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Assert.AreEqual(e.Message, "Start Date greater then End Date");
             }
+        }
+        [Test]
+        public void WhenGiven_StartDate_And_EndDate_To_FindCheapestTotalRate_ShouldReturn_CheapestTotalRate()
+        {
+            int expected = 200;
+            HotelDetails hotelDetailsTestObj = new HotelDetails();
+            hotelDetailsTestObj.AddHotel("Lakewood", 110, 90);
+            hotelDetailsTestObj.AddHotel("Bridgewood", 150, 50);
+            hotelDetailsTestObj.AddHotel("Ridgewood", 220, 150);
+            HotelReservation hotelReservationTestOj = new HotelReservation();
+            int result = hotelReservationTestOj.FindCheapestTotalRate(Convert.ToDateTime("11/09/2020"), Convert.ToDateTime("12/09/2020"));
+            Assert.AreEqual(expected, result);
         }
     }
 }
